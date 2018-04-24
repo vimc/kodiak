0ef6d8c5e64b31e90ca6de0a9ba44fa081545d6f
Alex Hill <alex.hill@gmail.com>
2018-04-24 14:10:02 +0100

    use log4j to write to console

--- src/main/kotlin/org/vaccineimpact/kodiak/Main.kt
+++ src/main/kotlin/org/vaccineimpact/kodiak/Main.kt
@@ -2,36 +2,55 @@ package org.vaccineimpact.kodiak
 
 import org.slf4j.LoggerFactory
 
-fun main(args: Array<String>) {
+val logger = LoggerFactory.getLogger(Kodiak::class.java)!!
 
-    val config = Config()
-    Kodiak().init()
-    println("Available targets: ${config.targets.map{ it.id }}")
+fun main(args: Array<String>) {
 
     val allowedModes = arrayOf("backup", "restore", "init")
 
     if (args.isEmpty() || !allowedModes.contains(args[0])) {
-        println("Please provide a command-line argument of either 'backup', 'restore' or 'init")
+        logger.info("Please provide a command-line argument of either 'backup', 'restore' or 'init")
         return
     }
 
+    val kodiak = Kodiak(args.drop(1))
     if (args[0] == "init"){
-        if (args.count() == 1) {
-            println("Please provide at least one target")
+        kodiak.init()
+    }
+    if (args[0] == "backup"){
+        kodiak.backup()
+    }
+    if (args[0] == "restore"){
+        kodiak.restore()
+    }
+}
+
+class Kodiak(private val targets: List<String>,
+             private val config: Config = Config()) {
+
+    private fun requireTargets() {
+        if (targets.any()) {
+            logger.info("Please provide at least one target")
+            logger.info("Available targets: ${config.targets.map{ it.id }}")
+
             return
         }
 
-        val targets = args.drop(1)
         println("Chosen targets: ${targets.map{ it }}")
     }
 
-    println("TODO: ${args[0]}")
-}
-
-class Kodiak {
-    private val logger = LoggerFactory.getLogger(Kodiak::class.java)
-
     fun init() {
-        logger.debug("Hello world")
+        logger.info("init")
+        requireTargets()
+    }
+
+    fun backup() {
+        logger.info("backup")
+        requireTargets()
+    }
+
+    fun restore() {
+        logger.info("restore")
+        requireTargets()
     }
 }
