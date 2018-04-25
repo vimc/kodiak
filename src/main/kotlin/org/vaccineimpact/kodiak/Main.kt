759ac5ccf345a229f5a1f69abd6661eb214a62c3
Alex Hill <alex.hill@gmail.com>
2018-04-25 13:15:27 +0100

    wip

--- src/main/kotlin/org/vaccineimpact/kodiak/Main.kt
+++ src/main/kotlin/org/vaccineimpact/kodiak/Main.kt
@@ -1,20 +1,42 @@
 package org.vaccineimpact.kodiak
 
+import org.docopt.Docopt
+import org.slf4j.LoggerFactory
+
+val logger = LoggerFactory.getLogger(Kodiak::class.java)!!
+
+private val doc = (
+                "Usage:\n"
+                + "  kodiak init\n"
+                + "  kodiak backup\n"
+                + "  kodiak restore")
+
 fun main(args: Array<String>) {
 
-    val config = JsonConfig()
-    println("Available targets: ${config.targets.map{ it.id }}")
+    val opts = Docopt(doc)
+            .parse(args.toList())
 
-    val allowedModes = arrayOf("backup", "restore", "init")
+    val kodiak = Kodiak(args.drop(1))
 
-    if (args.isEmpty() || !allowedModes.contains(args[0])) {
-        println("Please provide a command-line argument of either 'backup', 'restore' or 'init")
-        return
+    if (opts["init"] as Boolean){
+        kodiak.init()
     }
+    if (opts["backup"] as Boolean){
+        kodiak.backup()
+    }
+    if (opts["restore"] as Boolean){
+        kodiak.restore()
+    }
+}
 
-    if (args[0] == "init"){
-        if (args.count() == 1) {
-            println("Please provide at least one target")
+class Kodiak(private val targets: List<String>,
+             private val config: Config = JsonConfig()) {
+
+    private fun requireTargets() {
+        if (targets.any()) {
+            logger.info("Please provide at least one target")
+            logger.info("Available targets: ${config.targets.map{ it.id }}")
+
             return
         }
 
