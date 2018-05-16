b8436c7a85bdff4bdb28946af73fc87331e8e716
Alex Hill <alex.hill@gmail.com>
2018-05-16 18:57:55 +0100

    wip

--- src/main/kotlin/org/vaccineimpact/kodiak/EnvironmentProperties.kt
+++ src/main/kotlin/org/vaccineimpact/kodiak/EnvironmentProperties.kt
@@ -1,14 +1,19 @@
 package org.vaccineimpact.kodiak
 
+import org.slf4j.LoggerFactory
+
 class EnvironmentProperties {
 
-    val production = System.getProperty("production", "false").toBoolean()
+    var configSource: String = "/etc/kodiak/config.json"
 
-    val configSource: String = if (production) {
-        "/etc/kodiak/config.json"
-    } else {
-        EnvironmentProperties::class.java.classLoader
-                .getResource("config.json")
-                .path
+    init {
+        val dev = System.getProperty("dev", "false").toBoolean()
+        LoggerFactory.getLogger(EnvironmentProperties::class.java)
+                .info(System.getenv("production"))
+        if (dev) {
+            configSource = EnvironmentProperties::class.java.classLoader
+                    .getResource("config.json")
+                    .path
+        }
     }
 }
