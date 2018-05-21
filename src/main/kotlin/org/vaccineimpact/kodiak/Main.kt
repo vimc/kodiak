6d25ff4fe3e6066595c26ff6b9674dcb4dbb0dd1
Alex Hill <alex.hill@gmail.com>
2018-05-21 14:28:13 +0100

    use multiline string

--- src/main/kotlin/org/vaccineimpact/kodiak/Main.kt
+++ src/main/kotlin/org/vaccineimpact/kodiak/Main.kt
@@ -5,10 +5,10 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 private val doc = (
-                "Usage:\n"
-                + "  kodiak init [TARGET...]\n"
-                + "  kodiak backup\n"
-                + "  kodiak restore")
+        """Usage:
+        |  kodiak init [TARGET...]
+        |  kodiak backup
+        |  kodiak restore""".trimMargin())
 
 
 fun main(args: Array<String>) {
@@ -27,14 +27,14 @@ class Kodiak(private val config: Config,
 
     fun main(opts: Map<String, Any>) {
 
-        if (opts["init"] as Boolean){
+        if (opts["init"] as Boolean) {
             @Suppress("UNCHECKED_CAST")
             init(opts["TARGET"] as ArrayList<String>)
         }
-        if (opts["backup"] as Boolean){
+        if (opts["backup"] as Boolean) {
             backup()
         }
-        if (opts["restore"] as Boolean){
+        if (opts["restore"] as Boolean) {
             restore()
         }
     }
@@ -42,12 +42,12 @@ class Kodiak(private val config: Config,
     private fun requireTargets(targets: List<String>) {
         if (!targets.any()) {
             logger.info("Please provide at least one target")
-            logger.info("Available targets: ${config.targets.joinToString{ it.id }}")
+            logger.info("Available targets: ${config.targets.joinToString { it.id }}")
 
             return
         }
 
-        logger.info("Chosen targets: ${targets.joinToString{ it }}")
+        logger.info("Chosen targets: ${targets.joinToString { it }}")
     }
 
     fun init(targets: List<String>) {
